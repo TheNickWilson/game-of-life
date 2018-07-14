aa3de9b2cda21611e64a349d8c0f4f11bde7a301
Nick Wilson <thenickwilson@gmail.com>
2018-07-14 12:56:01 +0100

    Making our test arbitrary
    Now that we have our `set` method, we can make the test arbitrary.
    To do this we generate an arbitrary size, create a board of that
    size with arbitrary cells, and pick arbitrary x and y
    coordinates which gets set to being Dead.  We can then test
    that this cell stays Dead on the next tick.
    
    However, now that our board is bigger, finding how many living
    neighbours our cell has is a bit trickier. The approach we take is
    to look for the 3x3 grid centered on our target cell; but of course
    some of these won't exist if our cell is on an edge or a corner.  So
    we lit them into Options and discard any `None`'s.
    
    This bit of code - finding a cell's neighbours - immediately looks
    like another really useful piece of code, and one that our production
    code is likely to use.  Likewise, the code to get a cell's value when
    we don't know if its coordinates are on the board or not looks like
    a candidate to split out.

--- src/test/scala/GameOfLifeSpec.scala
+++ src/test/scala/GameOfLifeSpec.scala
@@ -1,9 +1,11 @@
-import org.scalacheck.{Arbitrary, Gen}
+import org.scalacheck.{Arbitrary, Gen, Shrink}
 import org.scalacheck.Arbitrary.arbitrary
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatest.{FreeSpec, MustMatchers}
 
 class GameOfLifeSpec extends FreeSpec with MustMatchers with GeneratorDrivenPropertyChecks {
+
+  implicit def noShrink[A]: Shrink[A] = Shrink.shrinkAny
 
   implicit lazy val arbitraryCell: Arbitrary[Cell] =
     Arbitrary {
@@ -18,28 +20,40 @@ class GameOfLifeSpec extends FreeSpec with MustMatchers with GeneratorDrivenProp
 
   def mainlyDeadCells = frequencyCell(2, 1)
 
+  def genGameOfLife(size: Int, genCell: Gen[Cell] = arbitrary[Cell]): Gen[GameOfLife] =
+    Gen.listOfN(size, Gen.listOfN(size, genCell))
+      .map(s => GameOfLife(s))
+
   "a dead cell" - {
 
     "must stay dead" - {
 
       "when it has 2 or fewer living neighbours" in {
 
-        val gameGen: Gen[Seq[Seq[Cell]]] =
-          Gen.listOfN(3, Gen.listOfN(3, mainlyDeadCells))
+        val gen =
+          for {
+            size <- Gen.chooseNum(3, 100)
+            game <- genGameOfLife(size, mainlyDeadCells)
+            x    <- Gen.chooseNum(0, size - 1)
+            y    <- Gen.chooseNum(0, size - 1)
+          } yield (game.set(x, y, Dead), x, y)
 
-        forAll(gameGen) {
-          state =>
+        forAll(gen) {
+          case (game, x, y) =>
 
-            val middleRow = state(1)
-            val patchedMiddleRow = middleRow.patch(1, Seq(Dead), 1)
-            val newState = state.patch(1, Seq(patchedMiddleRow), 1)
+            val neighbours = {
+              for {
+                xs <- (x - 1) to (x + 1)
+                ys <- (y - 1) to (y + 1)
+                if xs != x || ys != y
+              } yield game.state.lift(ys).flatMap(_.lift(xs))
+            }.flatten.toList
 
-            val numberOfLivingCells =
-              newState.map(_.count(_ == Alive)).sum
+            val numberOfLivingNeighbours =
+              neighbours.count(_ == Alive)
 
-            whenever(numberOfLivingCells <= 2) {
-              val game = GameOfLife(newState)
-              game.next.state(1)(1) mustEqual Dead
+            whenever(numberOfLivingNeighbours <= 2) {
+              game.next.state(y)(x) mustEqual Dead
             }
         }
       }
@@ -54,8 +68,7 @@ class GameOfLifeSpec extends FreeSpec with MustMatchers with GeneratorDrivenProp
         for {
           cell  <- arbitrary[Cell]
           size  <- Gen.chooseNum(1, 100)
-          game  <- Gen.listOfN(size, Gen.listOfN(size, arbitrary[Cell]))
-                    .map(s => GameOfLife(s))
+          game  <- genGameOfLife(size)
           x     <- Gen.chooseNum(0, size - 1)
           y     <- Gen.chooseNum(0, size - 1)
         } yield (game, cell, x, y)
